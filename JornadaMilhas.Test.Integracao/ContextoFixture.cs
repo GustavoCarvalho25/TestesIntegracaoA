c6e660c55ce7e29f1ab0308339ddf269c2477d42
Gustavo Carvalho Alves dos Santos <gustavo.carvalho@treetech.com.br>
2024-07-23 23:56:50 -0300

    Atualizando ContextoFixture para alcançar resultado correto de teste

--- JornadaMilhas.Test.Integracao/ContextoFixture.cs
+++ JornadaMilhas.Test.Integracao/ContextoFixture.cs
@@ -1,5 +1,8 @@
-﻿using JornadaMilhas.Dados;
+﻿using Bogus;
+using JornadaMilhas.Dados;
+using JornadaMilhasV1.Modelos;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,29 +10,53 @@ using System.Text;
 using System.Threading.Tasks;
 using Testcontainers.MsSql;
 
-namespace JornadaMilhas.Test.Integracao
+namespace JornadaMilhas.Test.Integracao;
+public class ContextoFixture : IAsyncLifetime
 {
-    //Possibilidades: ClassFixture, CollectionFixture e AssemblyFixture
-    public class ContextoFixture : IAsyncLifetime
-    {
-        public JornadaMilhasContext Context { get; set; }
-        private readonly MsSqlContainer _msSqlContainer = new MsSqlBuilder()
-            .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
-            .Build();
+    public JornadaMilhasContext Context { get; private set; }
+    private readonly MsSqlContainer _msSqlContainer = new MsSqlBuilder()
+        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+        .Build();
 
-        public async Task InitializeAsync()
-        {   
-            await _msSqlContainer.StartAsync();
-            var options = new DbContextOptionsBuilder<JornadaMilhasContext>()
+    public async Task InitializeAsync()
+    {
+        await _msSqlContainer.StartAsync();
+        var options = new DbContextOptionsBuilder<JornadaMilhasContext>()
             .UseSqlServer(_msSqlContainer.GetConnectionString())
             .Options;
-            Context = new JornadaMilhasContext(options);
-            Context.Database.Migrate();
-        }
 
-        public async Task DisposeAsync()
-        {
-            await _msSqlContainer.StopAsync();
-        }
+        Context = new JornadaMilhasContext(options);
+        Context.Database.Migrate();
+    }
+
+    public void CriaDadosFake()
+    {
+        var fakerPeriodo = new Faker<Periodo>()
+            .CustomInstantiator(f =>
+            {
+                DateTime dataInicio = f.Date.Soon();
+                return new Periodo(dataInicio, dataInicio.AddDays(30));
+            });
+
+        var rota = new Rota("Curitiba", "São Paulo");
+
+        var fakerOferta = new Faker<OfertaViagem>()
+            .CustomInstantiator(f => new OfertaViagem(
+                rota,
+                fakerPeriodo.Generate(),
+                100 * f.Random.Int(1, 100))
+            )
+            .RuleFor(o => o.Desconto, f => 40)
+            .RuleFor(o => o.Ativa, f => true);
+
+        var lista = fakerOferta.Generate(200);
+        Context.OfertasViagem.AddRange(lista);
+        Context.SaveChanges();
+
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _msSqlContainer.StopAsync();
     }
 }
